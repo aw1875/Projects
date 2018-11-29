e01807a5667c3189b9a41d6720968c2eb12b10f3
Adam Wolf <aw1875@rit.edu>
2018-11-29 14:23:25 -0500

    Completed Route

---
+++ Project 2/src/stu/Route.java
@@ -0,0 +1,64 @@
+package stu;
+
+public class Route {
+
+    private int id;
+    private String starting;
+    private String ending;
+    private int distance;
+
+
+    /**
+     * Take in each of the private states declared and set them
+     * @param id
+     * @param starting
+     * @param ending
+     * @param distance
+     */
+    public Route(int id, String starting, String ending, int distance) {
+        this.id = id;
+        this.starting = starting;
+        this.ending = ending;
+        this.distance = distance;
+    }
+
+    /**
+     * Get int id
+     * @return id
+     */
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Get String starting
+     * @return starting
+     */
+    public String getStart() {
+        return this.starting;
+    }
+
+    /**
+     * Get String ending
+     * @return ending
+     */
+    public String getEnd() {
+        return this.ending;
+    }
+
+    /**
+     * Get int distance
+     * @return distance
+     */
+    public int getDistance() {
+        return this.distance;
+    }
+
+    /**
+     * Print out the class
+     * @return class as a string
+     */
+    public String toString() {
+        return this.id + ": " + this.starting + " to " + this.ending + ", " + this.distance + " distance";
+    }
+}
